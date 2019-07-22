35446537e17d7e9ab3152151aed8bd60329eebda
grujic96 <aleksandar.grujicc@gmail.com>
2019-07-22 22:29:14 +0200

    .

---
+++ universityapi/src/main/java/com/nisuniversity/spring/configuration/SecurityJavaConfig.java
@@ -0,0 +1,61 @@
+package com.nisuniversity.spring.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.nisuniversity.spring.security.RestAuthEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+
+	
+    @Autowired private RestAuthEntryPoint authenticationEntryPoint;
+	
+	@Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+        	.withUser("test")
+        	.password("testpwd")
+        	.authorities("USER");
+
+    }
+
+    // Secure the endpoins with HTTP Basic authentication
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+        .authorizeRequests().antMatchers("/").permitAll().and()
+    	.authorizeRequests().antMatchers("/api/students").authenticated().and()
+//    	.authorizeRequests().antMatchers("/api/**").authenticated().and()
+//        .formLogin().loginProcessingUrl("/login").and()
+        .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+        .and().csrf().disable();
+      
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    /*@Bean
+    public UserDetailsService userDetailsService() {
+        //ok for demo
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("user").password("password").roles("USER").build());
+        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
+        return manager;
+    }*/
+
+}
